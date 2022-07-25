e5922fc50f0b6288f6c2e56bbe8f5c5b2bacc9e5
rmoc99 <rui.cunh4@gmail.com>
2022-07-25 13:25:57 +0100

    heroku

--- src/App.js
+++ src/App.js
@@ -2,24 +2,8 @@ import * as React from 'react'
 import './App.css';
 import Image1 from './main.jpg'
 import Image2 from './pizzaWallpaper.jpg'
-import {ChakraProvider, Box, Image, Badge, Heading, Text,Link } from '@chakra-ui/react'
-const store1 = {
-    imageUrl: Image1,
-    imageAlt: Image1,
-    atribute1: 'Peça já',
-    badge: 'Novo',
-    title: 'Good Burger',
-    link: 'https://ps-burger.herokuapp.com/',
-}
-const store2 = {
-    imageUrl: Image2,
-    imageAlt: Image2,
-    atribute1: 'Biggest Pizza',
-    badge: 'Pomoções',
-    title: 'Fast Pizza',
-    link: 'https://fast-pizza-client.herokuapp.com/',
-}
-const stores = [store1, store2]
+import {ChakraProvider, Box, Image, Heading, Text,Link } from '@chakra-ui/react'
+
 function App() {
 
   return (
@@ -36,31 +20,27 @@ function App() {
                 </Text>
             </Heading>
             <Box display='flex'>
-                {stores.map((store) => {
-                    return (
-                        <>  
-                        <Link href={store.link}>
+                        <Link href='https://ps-burger.herokuapp.com/'>
                             <Box cursor='pointer' maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden' marginRight='50px'>
-                                <Image src={store.imageUrl} alt={store.alt} />
+                                <Image src={Image1} alt={Image1} />
                                 <Box p='6'>
-                                    <Box display='flex' alignItems='baseline'>
-                                        <Badge borderRadius='full' px='2' colorScheme='teal'>
-                                            {store.badge}
-                                        </Badge>
-                                        <Box marginLeft='5px' color='gray.500' fontWeight='semibold' letterSpacing='wide' fontSize='xs' textTransform='uppercase'>
-                                            {store.atribute1}
-                                        </Box>
-                                    </Box>
                                     <Box mt='1' fontWeight='semibold' as='h4' lineHeight='tight' noOfLines={1}>
-                                        {store.title}
+                                        Good Burger
                                     </Box>
                                 </Box>
                             </Box>
                             </Link>
-                        </>
-                    )
-                }
-                )}
+
+                            <Link href='https://fast-pizza-client.herokuapp.com/'>
+                            <Box cursor='pointer' maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden' marginRight='50px'>
+                                <Image src={Image2} alt={Image2} />
+                                <Box p='6'>
+                                    <Box mt='1' fontWeight='semibold' as='h4' lineHeight='tight' noOfLines={1}>
+                                        Fast Pizza
+                                    </Box>
+                                </Box>
+                            </Box>
+                            </Link>
             </Box>
         </div>
     </ChakraProvider>
