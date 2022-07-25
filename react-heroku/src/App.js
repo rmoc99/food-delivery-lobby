b45da3d5b39ed94cc7a2b6f9a6facace98d736df
rmoc99 <rui.cunh4@gmail.com>
2022-07-25 10:54:10 +0100

    remove gitignore files

--- react-heroku/src/App.js
+++ react-heroku/src/App.js
@@ -1,8 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { ChakraProvider } from '@chakra-ui/react'
 function App() {
   return (
+    <ChakraProvider>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +20,7 @@ function App() {
         </a>
       </header>
     </div>
+    </ChakraProvider>
   );
 }
 
